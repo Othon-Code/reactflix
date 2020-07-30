492e6602c9e5c701771e80ef6e865898a90615f3
Othon-Code <othonn.ssousa@gmail.com>
2020-07-30 16:51:57 -0300

    Comit da tela de cadastro

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Menu from './components/Menu';
-import dadosIniciais from './components/data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
+import Menu from '../../components/Menu';
+import dadosIniciais from '../../components/data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
 
-function App() {
+function Home() {
   return (
     <div style={{ background:"#141414"}}>
       <Menu />
@@ -41,4 +41,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
